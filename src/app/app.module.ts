91181c4d2fabc8956fe61162f0fed861bae74ac2
Ignacio Montes Álvarez <nachonava98@gmail.com>
2019-02-24 13:28:20 +0100

    Added solid-generator based login + register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,85 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {LoginPopupComponent} from './login-popup/login-popup.component';
+import {LoginComponent} from './login/login.component';
+import { CardComponent } from './card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+// Services
+import { AuthService } from './services/solid.auth.service';
+import { AuthGuard } from './services/auth.guard.service';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatFormComponent } from './chat-form/chat-form.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
 import { FeedComponent } from './feed/feed.component';
 import { MessageComponent } from './message/message.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { UserListComponent } from './user-list/user-list.component';
 import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'login-popup',
+    component: LoginPopupComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'card',
+    component: CardComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    LoginPopupComponent,
+    DashboardComponent,
+    CardComponent,
+    RegisterComponent,
     ChatFormComponent,
     ChatroomComponent,
     FeedComponent,
     MessageComponent,
-    LoginFormComponent,
-    SignupFormComponent,
     NavbarComponent,
-    UserListComponent,
-    UserItemComponent
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(routes),
+    NgSelectModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule //required for toastr
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

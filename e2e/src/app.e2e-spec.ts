f25eba679982a3597ac2d7417d83585afad8a579
Miguel Garnacho Velez <migarve55@gmail.com>
2019-02-27 10:34:06 +0100

    Fixing e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to solid-app!');
+    expect(page.getParagraphText()).toEqual('PROFILE DEMO APP');
   });
 });

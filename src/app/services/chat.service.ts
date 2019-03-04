39d8c782ed0fc82908b9c0fea7fe6ea1f47cb485
Ignacio Montes Álvarez <nachonava98@gmail.com>
2019-03-04 19:49:13 +0100

    Added friends listing with photo

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -11,22 +11,28 @@ export class ChatService {
 
   chatMessages: ChatMessage[] = new Array<ChatMessage>();
   userName: string;
+  friends: Array<User> = new Array<User>();
 
   constructor(private rdf : RdfService) {
     this.rdf.getProfile().then(response => this.userName = response.fn);
-    console.log(this.rdf.getValueFromFoaf("knows"));
+    this.loadFriends();
   }
 
+  // TODO: at this time, profile pic for the main user is just an empty path, maybe we can fix this
   getUser() {
-    return of(new User(this.userName));
+    return of(new User(this.userName, ""));
   }
 
   getUsers() : Observable<User[]> {
-    var users = new Array<User>();
-    users.push(new User("Fulanito"));
-    users.push(new User("Menganito"));
-    users.push(new User("Adolfito"));
-    return of(users);
+    return of(this.friends);
+  }
+
+  private async loadFriends() {
+    await this.rdf.getSession();
+    (await this.rdf.getFriends()).forEach(async element => {
+      await this.rdf.fetcher.load(element.value);
+      this.friends.push(new User(this.rdf.getValueFromVcard('fn', element.value), this.rdf.getValueFromVcard('hasPhoto', element.value)));
+    });
   }
 
   sendMessage(msg: string) {

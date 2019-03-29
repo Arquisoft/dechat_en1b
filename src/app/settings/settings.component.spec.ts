64f7d84c9261b6095da8200f24be042fbfd6ef5a
Nacho <nachonava98@gmail.com>
2019-03-29 10:52:54 +0100

    Added tests for the settings component

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -4,6 +4,8 @@ import { SettingsComponent } from './settings.component';
 import { FormsModule } from '@angular/forms';
 import { ChatService } from '../services/chat.service';
 import { ToastrService, ToastrModule } from 'ngx-toastr';
+import {By} from '@angular/platform-browser';
+import {EventEmitter} from '@angular/core';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -26,5 +28,41 @@ describe('SettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display input error (add)', () => {
+    component.webIdAddFriend = null;
+    component.addFriend();
+    expect(By.css('#toastr-container'));
+
+    component.webIdAddFriend = '';
+    component.addFriend();
+    expect(By.css('#toastr-container'));
+
+    // TODO: check that you are in session to make sure that it doesn't crash
+    // component.webIdAddFriend = 'test';
+    // component.addFriend();
+    // expect(!By.css('#toastr-container'));
+  });
+
+  it('should display input error (remove)', () => {
+    component.webIdRemoveFriend = null;
+    component.addFriend();
+    expect(By.css('#toastr-container'));
+
+    component.webIdRemoveFriend = '';
+    component.addFriend();
+    expect(By.css('#toastr-container'));
+
+    // TODO: check that you are in session to make sure that it doesn't crash
+    // component.webIdRemoveFriend = 'test';
+    // component.removeFriend();
+    // expect(!By.css('#toastr-container'));
+  });
+
+  it('should click intro', () => {
+    component.webIdAddFriend = null;
+    component.handleSubmit({keyCode: 13});
+    expect(By.css('#toastr-container'));
   });
 });

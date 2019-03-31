14c245f660ec827e4f7e78803665e410d51efbbf
Pablo Menendez Fernandez <uo257971@uniovi.es>
2019-03-31 16:59:50 +0200

    Test Settings

--- src/app/settings/settings.component.spec.ts
+++ src/app/settings/settings.component.spec.ts
@@ -1,13 +1,16 @@
+    
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsComponent } from './settings.component';
 import { FormsModule } from '@angular/forms';
 import { ChatService } from '../services/chat.service';
 import { ToastrService, ToastrModule } from 'ngx-toastr';
+import {By} from '@angular/platform-browser';
 
 describe('SettingsComponent', () => {
-  /*let component: SettingsComponent;
+  let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
+  let spy: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -26,5 +29,50 @@ describe('SettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });*/
+  });
+
+  it('should display input error (add)', () => {
+
+    /*spy = spyOn(component.chat, 'addFriend').and.callFake(function(friend) {
+      return null;
+    });*/
+
+    component.webIdAddFriend = 'test';
+    component.addFriend();
+    expect(!By.css('#toastr-container'));
+
+    component.webIdAddFriend = null;
+    component.addFriend();
+    expect(By.css('#toastr-container'));
+
+    component.webIdAddFriend = '';
+    component.addFriend();
+    expect(By.css('#toastr-container'));
+  });
+
+  it('should display input error (remove)', () => {
+
+    /*spy = spyOn(component.chat, 'removeFriend').and.callFake(function(friend) {
+      return null;
+    });*/
+
+    component.webIdRemoveFriend = 'test';
+    component.removeFriend();
+    expect(!By.css('#toastr-container'));
+
+    component.webIdRemoveFriend = null;
+    component.addFriend();
+    expect(By.css('#toastr-container'));
+
+    component.webIdRemoveFriend = '';
+    component.addFriend();
+    expect(By.css('#toastr-container'));
+
+  });
+
+  it('should click intro', () => {
+    component.webIdAddFriend = null;
+    component.handleSubmit({keyCode: 13});
+    expect(By.css('#toastr-container'));
+  });
 });

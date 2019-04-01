4f233532ab050f21a6045a102eee2825ba431253
Miguel Garnacho Velez <migarve55@gmail.com>
2019-04-01 17:50:24 +0200

    Base service chat

---
+++ src/app/services/chat.service.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ChatService } from './chat.service';
+import { RdfService } from './rdf.service';
+import { ToastrService, ToastrModule } from 'ngx-toastr';
+
+describe('ChatService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ ToastrModule.forRoot() ],
+    providers: [ ChatService ]
+  }));
+
+  it('should be created', () => {
+    const service: ChatService = TestBed.get(ChatService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should be created', () => {
+    const service: ChatService = TestBed.get(ChatService);
+
+  });
+
+});
